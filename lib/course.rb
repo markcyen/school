347edba55a01e7650a21e965788a9a66f49b23f6
MarkY <markcyen@yahoo.com>
2021-04-07 20:08:18 -0600

    Implement students and full? methods

--- lib/course.rb
+++ lib/course.rb
@@ -2,11 +2,17 @@ require './lib/student'
 
 class Course
   attr_reader :name,
-              :capacity
+              :capacity,
+              :students
 
   def initialize(name, capacity)
     @name = name
     @capacity = capacity
+    @students = []
   end
-  
+
+  def full?
+    @students.length > @capacity
+  end
+
 end
