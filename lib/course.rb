45db26a553ef3b47099571d1628920f8279f0883
MarkY <markcyen@yahoo.com>
2021-04-07 20:01:02 -0600

    Implement initialize method

---
+++ lib/course.rb
@@ -0,0 +1,12 @@
+require './lib/student'
+
+class Course
+  attr_reader :name,
+              :capacity
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+  end
+  
+end
