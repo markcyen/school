94e28e93a7f1892576dd8461dba5ede9c9b2e203
MarkY <markcyen@yahoo.com>
2021-04-07 19:41:26 -0600

    Create test for scores methods

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -14,5 +14,26 @@ RSpec.describe Student do
       expect(student.age).to eq(21)
     end
   end
-  
+
+  context 'check for scores' do
+    it '#scores is empty' do
+      student = Student.new({name: "Morgan", age: 21})
+      expect(student.scores).to eq([])
+    end
+
+    it '#log_scores into array' do
+      student = Student.new({name: "Morgan", age: 21})
+      student.log_scores(89)
+      student.log_scores(78)
+      expect(student.scores).to eq([89, 78])
+    end
+
+    it '#grade as average of scores' do
+      student = Student.new({name: "Morgan", age: 21})
+      student.log_scores(89)
+      student.log_scores(78)
+      expect(student.grade).to eq(83.5)
+    end
+  end
+
 end
