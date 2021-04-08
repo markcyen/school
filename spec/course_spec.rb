eb1de3d42235f3226b09f1076049adaf07cf0c4f
MarkY <markcyen@yahoo.com>
2021-04-07 19:57:36 -0600

    Create initialize test

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -1,1 +1,18 @@
+require 'rspec'
+require './lib/course'
+require './lib/student'
 
+RSpec.describe Course do
+  context '#initialize' do
+    it 'exists' do
+      course = Course.new("Calculus", 2)
+      expect(course).to be_instance_of(Course)
+    end
+
+    it 'names the course and capacity' do
+      course = Course.new("Calculus", 2)
+      expect(course.name).to eq("Calculus")
+      expect(course.capacity).to eq(2)
+    end
+  end
+end
